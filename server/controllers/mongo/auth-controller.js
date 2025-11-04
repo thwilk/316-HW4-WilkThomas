ae959d70bcd15a4f9f032ebe8b77989e93e14174
thwilk <135527401+thwilk@users.noreply.github.com>
2025-11-04 11:41:14 -0500

    extracted auth logic

--- server/controllers/mongo/auth-controller.js
+++ server/controllers/mongo/auth-controller.js
@@ -1,20 +1,18 @@
-const auth = require('../../auth')
-const User = require('../../models/mongo/user-model')
-const bcrypt = require('bcryptjs')
+// server/controllers/mongo/store-controller.js
+const auth = require('../../auth');
+const userService = require('../../db/mongo/authdb');
 
-getLoggedIn = async (req, res) => {
+const getLoggedIn = async (req, res) => {
     try {
-        let userId = auth.verifyUser(req);
+        const userId = auth.verifyUser(req);
         if (!userId) {
-            return res.status(200).json({
-                loggedIn: false,
-                user: null,
-                errorMessage: "?"
-            })
+            return res.status(200).json({ loggedIn: false, user: null });
         }
 
-        const loggedInUser = await User.findOne({ _id: userId });
-        console.log("loggedInUser: " + loggedInUser);
+        const loggedInUser = await userService.findUserById(userId);
+        if (!loggedInUser) {
+            return res.status(400).json({ loggedIn: false, user: null });
+        }
 
         return res.status(200).json({
             loggedIn: true,
@@ -23,152 +21,90 @@ getLoggedIn = async (req, res) => {
                 lastName: loggedInUser.lastName,
                 email: loggedInUser.email
             }
-        })
+        });
     } catch (err) {
-        console.log("err: " + err);
-        res.json(false);
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
     }
-}
+};
 
-loginUser = async (req, res) => {
-    console.log("loginUser");
+const loginUser = async (req, res) => {
     try {
         const { email, password } = req.body;
-
         if (!email || !password) {
-            return res
-                .status(400)
-                .json({ errorMessage: "Please enter all required fields." });
+            return res.status(400).json({ errorMessage: "Please enter all required fields." });
         }
 
-        const existingUser = await User.findOne({ email: email });
-        console.log("existingUser: " + existingUser);
+        const existingUser = await userService.findUserByEmail(email);
         if (!existingUser) {
-            return res
-                .status(401)
-                .json({
-                    errorMessage: "Wrong email or password provided."
-                })
+            return res.status(401).json({ errorMessage: "Wrong email or password." });
         }
 
-        console.log("provided password: " + password);
-        const passwordCorrect = await bcrypt.compare(password, existingUser.passwordHash);
+        const passwordCorrect = await userService.verifyPassword(existingUser, password);
         if (!passwordCorrect) {
-            console.log("Incorrect password");
-            return res
-                .status(401)
-                .json({
-                    errorMessage: "Wrong email or password provided."
-                })
+            return res.status(401).json({ errorMessage: "Wrong email or password." });
         }
 
-        // LOGIN THE USER
         const token = auth.signToken(existingUser._id);
-        console.log(token);
-
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: true
-        }).status(200).json({
-            success: true,
-            user: {
-                firstName: existingUser.firstName,
-                lastName: existingUser.lastName,  
-                email: existingUser.email              
-            }
-        })
-
+        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: true })
+            .status(200).json({
+                success: true,
+                user: {
+                    firstName: existingUser.firstName,
+                    lastName: existingUser.lastName,
+                    email: existingUser.email
+                }
+            });
     } catch (err) {
         console.error(err);
         res.status(500).send();
     }
-}
+};
 
-logoutUser = async (req, res) => {
-    res.cookie("token", "", {
-        httpOnly: true,
-        expires: new Date(0),
-        secure: true,
-        sameSite: "none"
-    }).send();
-}
+const logoutUser = async (req, res) => {
+    res.cookie("token", "", { httpOnly: true, expires: new Date(0), secure: true, sameSite: "none" }).send();
+};
 
-registerUser = async (req, res) => {
-    console.log("REGISTERING USER IN BACKEND");
+const registerUser = async (req, res) => {
     try {
         const { firstName, lastName, email, password, passwordVerify } = req.body;
-        console.log("create user: " + firstName + " " + lastName + " " + email + " " + password + " " + passwordVerify);
+
         if (!firstName || !lastName || !email || !password || !passwordVerify) {
-            return res
-                .status(400)
-                .json({ errorMessage: "Please enter all required fields." });
+            return res.status(400).json({ errorMessage: "Please enter all required fields." });
         }
-        console.log("all fields provided");
         if (password.length < 8) {
-            return res
-                .status(400)
-                .json({
-                    errorMessage: "Please enter a password of at least 8 characters."
-                });
+            return res.status(400).json({ errorMessage: "Password must be at least 8 characters." });
         }
-        console.log("password long enough");
         if (password !== passwordVerify) {
-            return res
-                .status(400)
-                .json({
-                    errorMessage: "Please enter the same password twice."
-                })
+            return res.status(400).json({ errorMessage: "Passwords do not match." });
         }
-        console.log("password and password verify match");
-        const existingUser = await User.findOne({ email: email });
-        console.log("existingUser: " + existingUser);
+
+        const existingUser = await userService.findUserByEmail(email);
         if (existingUser) {
-            return res
-                .status(400)
-                .json({
-                    success: false,
-                    errorMessage: "An account with this email address already exists."
-                })
+            return res.status(400).json({ errorMessage: "Email already exists." });
         }
 
-        const saltRounds = 10;
-        const salt = await bcrypt.genSalt(saltRounds);
-        const passwordHash = await bcrypt.hash(password, salt);
-        console.log("passwordHash: " + passwordHash);
-
-        const newUser = new User({firstName, lastName, email, passwordHash});
-        const savedUser = await newUser.save();
-        console.log("new user saved: " + savedUser._id);
-
-        // LOGIN THE USER
+        const savedUser = await userService.createUser({ firstName, lastName, email, password });
         const token = auth.signToken(savedUser._id);
-        console.log("token:" + token);
 
-        await res.cookie("token", token, {
-            httpOnly: true,
-            secure: true,
-            sameSite: "none"
-        }).status(200).json({
-            success: true,
-            user: {
-                firstName: savedUser.firstName,
-                lastName: savedUser.lastName,  
-                email: savedUser.email              
-            }
-        })
-
-        console.log("token sent");
-
+        res.cookie("token", token, { httpOnly: true, secure: true, sameSite: "none" })
+            .status(200).json({
+                success: true,
+                user: {
+                    firstName: savedUser.firstName,
+                    lastName: savedUser.lastName,
+                    email: savedUser.email
+                }
+            });
     } catch (err) {
         console.error(err);
         res.status(500).send();
     }
-}
+};
 
 module.exports = {
     getLoggedIn,
-    registerUser,
     loginUser,
-    logoutUser
-}
+    logoutUser,
+    registerUser
+};
